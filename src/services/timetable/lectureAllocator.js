fc575cffbfc9701dae2835c8ee32df847749eb0b
Atharve03 <atharve.bute@gmail.com>
2026-02-20 23:21:03 +0530

    initial changes

--- src/services/timetable/lectureAllocator.js
+++ src/services/timetable/lectureAllocator.js
@@ -1,54 +1,61 @@
 import {
-    hasTeacherConflict,
-    hasBatchConflict,
-    isWithinAvailability
-  } from "./conflictChecker.js";
-  
-  export default function allocateLectures({
-    teachers,
-    subjects,
-    availabilityMatrix,
-    days,
-    slotsPerDay,
-    timetable
-  }) {
-    const theorySubjects = subjects.filter((s) => !s.isLab);
-  
-    for (const sub of theorySubjects) {
-      const assignedTeachers = teachers.filter(
-        (t) => t.roleMap[sub._id] === "theory"
-      );
-  
-      if (assignedTeachers.length === 0) continue;
-  
+  hasTeacherConflict,
+  hasSectionConflict,
+  isWithinAvailability
+} from "./conflictChecker.js";
+
+export default function allocateLectures({
+  teachers,
+  subjects,
+  availabilityMatrix,
+  days,
+  slotsPerDay,
+  timetable
+}) {
+  const theorySubjects = subjects.filter((s) => !s.isLab);
+
+  for (const sub of theorySubjects) {
+
+    const assignedTeachers = teachers.filter(
+      (t) => t.roleMap[sub._id] === "theory"
+    );
+
+    if (!assignedTeachers.length) continue;
+
+    for (const teacher of assignedTeachers) {
+
       let remaining = sub.weeklyLectures;
-  
-      for (const teacher of assignedTeachers) {
-        for (const day of days) {
-          for (let slot = 1; slot <= slotsPerDay; slot++) {
-            if (remaining <= 0) break;
-  
-            if (hasTeacherConflict(timetable, day, slot, teacher._id)) continue;
-            if (!isWithinAvailability(availabilityMatrix, teacher._id, day, slot))
-              continue;
-  
-            timetable.push({
-              day,
-              slot,
-              subjectId: sub._id,
-              teacherId: teacher._id,
-              isLab: false,
-              batchId: null,
-              room: "ROOM-01"
-            });
-  
-            teacher.assignedLoad += 1;
-            remaining -= 1;
-          }
+
+      for (const day of days) {
+        for (let slot = 1; slot <= slotsPerDay; slot++) {
+
+          if (remaining <= 0) break;
+
+          
+          if (hasSectionConflict(timetable, day, slot, sub.sectionId)) continue;
+
+          if (hasTeacherConflict(timetable, day, slot, teacher._id)) continue;
+
+          if (!isWithinAvailability(availabilityMatrix, teacher._id, day, slot))
+            continue;
+
+          timetable.push({
+            sectionId: sub.sectionId,
+            batchId: null,
+            day,
+            slot,
+            subjectId: sub._id,
+            teacherId: teacher._id,
+            room: "ROOM-01",
+            isLab: false
+          });
+
+          teacher.assignedLoad += 1;
+          remaining--;
         }
       }
     }
-  
-    return timetable;
   }
-  
+
+  return timetable;
+}
